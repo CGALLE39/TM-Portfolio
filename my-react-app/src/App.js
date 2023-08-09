a4f99d0c86f6b4616f03b187967cfc3bea0aca2c
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-08-09 14:49:58 -0500

    navbar and pages

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import './component/About';
+import About from "./component/About";
+import React from "react";
+import Skills from "./component/Skills";
+import Navbar from "./component/Navbar";
+import Contact from "./component/Contact";
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+export default function App() {
+    return (
+        <main className="text-gray-400 bg-gray-900 body-font">
+            <Navbar />
+            <About />
+            <Skills />
+            <Contact />
+        </main>
+    );
 }
-
-export default App;
---
+++ my-react-app/src/component/Skills.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Skills() {
+    return (
+        <section id="skills" className="text-center">
+            <p className="">Education</p>
+            <br />
+            <p className="">University of Wisconsin Law School</p>
+            <p>Doctor of Law - JD, Law</p>
+            <br />
+            <p className="">Jilin University</p>
+            <p>Bachelor's degree, Social Sciences</p>
+            <br />
+            <p className="">License</p>
+            <br />
+            <p>Attorney</p>
+            <p>Supreme Court of Wisconsin</p>
+
+        </section>
+    )
+}
