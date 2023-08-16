b754364b8f62d26c68366591732d0518f0d639a0
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-08-16 12:46:19 -0500

    google form addition

--- my-react-app/src/component/Resources.jsx
+++ my-react-app/src/component/Resources.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Resources() {
-
+    return (
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSeBzPh8m_Skw6RWflpE_NsViL58Xw8fUTehVXJjp1hF1GXDBw/viewform?embedded=true"
+            width="640"
+            height="382"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0">
+            Loading…
+        </iframe>
+    )
 }
