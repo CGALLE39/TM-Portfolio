eb610be2f41100bde6395add4e9adf92351301f5
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-08-25 14:50:08 -0500

    email

--- my-react-app/src/component/Contact.jsx
+++ my-react-app/src/component/Contact.jsx
@@ -3,28 +3,36 @@ import emailjs from '@emailjs/browser';
 
 
 export default function Contact() {
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [message, setMessage] = React.useState("");
+    const form = useRef();
 
-    function encode(data) {
-        return Object.keys(data)
-            .map(
-                (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+    // const [name, setName] = React.useState("");
+    // const [email, setEmail] = React.useState("");
+    // const [message, setMessage] = React.useState("");
+    //
+    // function encode(data) {
+    //     return Object.keys(data)
+    //         .map(
+    //             (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+    //         )
+    //         .join("&");
+    // }
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm('service_00z6gdm', 'template_g9a9xrs', form.current, 'W7XtgWydGJXdU_qO_')
+            .then(
+                (result) => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                (error) => {
+                    alert('Failed to send the message, please try again')
+                }
             )
-            .join("&");
     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", name, email, message }),
-        })
-            .then(() => alert("Message sent!"))
-            .catch((error) => alert(error));
-    }
 
     return (
         <section id="contact" className="relative">
@@ -64,10 +72,8 @@ export default function Contact() {
                         </div>
                     </div>
                 </div>
-                <form
-                    netlify
-                    name="contact"
-                    onSubmit={handleSubmit}
+
+                <form ref={form} onSubmit={sendEmail}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                         Contact Me
@@ -80,12 +86,12 @@ export default function Contact() {
                             Name
                         </label>
                         <input
-                            type="text"
-                            id="name"
-                            name="name"
+                            type="email"
+                            name="user_email"
+                            placeholder="Email"
+                            required
                             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setName(e.target.value)}
-                        />
+                            />
                     </div>
                     <div className="relative mb-4">
                         <label htmlFor="email" className="leading-7 text-sm text-gray-400">
@@ -118,6 +124,7 @@ export default function Contact() {
                         Submit
                     </button>
                 </form>
+
             </div>
         </section>
     );
