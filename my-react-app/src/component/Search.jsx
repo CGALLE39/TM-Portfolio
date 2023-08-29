9661086249c27cf2ec4e56146034d2c103f8b7ad
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-08-29 13:10:42 -0500

    search tm

--- my-react-app/src/component/Search.jsx
+++ my-react-app/src/component/Search.jsx
@@ -1,32 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
+
 function Search() {
-    const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
+    //
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         try {
+    //             const response = await fetch('https://assignment-api.uspto.gov/documentation-trademark/#!/basicSearch/basicSearch');
+    //             const jsonData = await response.json();
+    //             setData(jsonData);
+    //         } catch (error) {
+    //             console.error('Error fetching data:', error);
+    //         }
+    //     }
+    //
+    //     fetchData();
+    // }, []);
+    //
+    // return (
+    //     <div className="App">
+    //         <h1>API Data</h1>
+    //         <ul>
+    //             {data.map(item => (
+    //                 <li key={item.id}>{item.name}</li>
+    //             ))}
+    //         </ul>
+    //     </div>
+    // );
 
     useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch('https://assignment-api.uspto.gov/documentation-trademark/#!/basicSearch/basicSearch');
-                const jsonData = await response.json();
-                setData(jsonData);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        }
-
-        fetchData();
-    }, []);
-
+        fetch('https://assignment-api.uspto.gov/documentation-trademark/#!/basicSearch/basicSearch')
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+    },[])
     return (
-        <div className="App">
-            <h1>API Data</h1>
-            <ul>
-                {data.map(item => (
-                    <li key={item.id}>{item.name}</li>
-                ))}
-            </ul>
+        <div className='search'>
+            <input type="text" placeholder='search here...' />
         </div>
-    );
+    )
 }
 
 export default Search;
