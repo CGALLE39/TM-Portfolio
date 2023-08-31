e590f852d92aab4201d791af10efa50bbc065301
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-08-31 15:51:13 -0500

    tm search

--- my-react-app/src/component/Search.jsx
+++ my-react-app/src/component/Search.jsx
@@ -2,35 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 
 function Search() {
-    // const [data, setData] = useState([]);
-    //
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         try {
-    //             const response = await fetch('https://assignment-api.uspto.gov/documentation-trademark/#!/basicSearch/basicSearch');
-    //             const jsonData = await response.json();
-    //             setData(jsonData);
-    //         } catch (error) {
-    //             console.error('Error fetching data:', error);
-    //         }
-    //     }
-    //
-    //     fetchData();
-    // }, []);
-    //
-    // return (
-    //     <div className="App">
-    //         <h1>API Data</h1>
-    //         <ul>
-    //             {data.map(item => (
-    //                 <li key={item.id}>{item.name}</li>
-    //             ))}
-    //         </ul>
-    //     </div>
-    // );
 
     useEffect(() => {
-        fetch('https://assignment-api.uspto.gov/documentation-trademark/#!/basicSearch/basicSearch')
+        fetch('https://bulkdata.uspto.gov/BDSS-API-SWAGGER/')
             .then(res => res.json())
             .then(data => console.log(data))
             .catch(err => console.log(err));
