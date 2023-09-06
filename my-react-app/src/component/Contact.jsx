d6331e67fe2b78e8c48a7f43c6f24c8e9fb6d61d
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-09-06 15:09:46 -0500

    google forms and additional edits

--- my-react-app/src/component/Contact.jsx
+++ my-react-app/src/component/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
 
     return (
         <section id="contact" className="relative">
-            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+            <div className="container px-5 py-10 mt-8 mx-auto flex sm:flex-nowrap flex-wrap">
                 <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
                     <iframe
                         width="100%"
@@ -38,7 +38,7 @@ export default function Contact() {
                         style={{ filter: "opacity(0.7)" }}
                         src="https://www.google.com/maps/embed/v1/place?q=Austin+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
                     />
-                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+                    <div className="bg-gray-900 relative flex flex-wrap py-6 px-8 rounded shadow-md">
                         <div className="lg:w-1/2 px-6">
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 ADDRESS
