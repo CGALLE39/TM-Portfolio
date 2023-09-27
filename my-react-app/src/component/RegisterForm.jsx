8947a079e716718ec45e89093e83fdcd5cc9af6e
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-09-27 10:39:40 -0500

    survey.js

--- my-react-app/src/component/RegisterForm.jsx
+++ my-react-app/src/component/RegisterForm.jsx
@@ -2,59 +2,104 @@ import React, { useState } from 'react';
 
 function MyForm() {
     // Initialize state variables for form fields
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: '',
-    });
+    const [selectedOption, setSelectedOption] = useState('business');
 
-    // Handle form input changes
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
+    // Function to handle radio button selection
+    const handleOptionChange = (e) => {
+        setSelectedOption(e.target.value);
     };
 
-    // Handle form submission
+    // Function to handle form submission
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Here, you can send the form data to a server, save it locally, or perform any other action.
-        console.log('Form data:', formData);
+        // You can perform actions with the selectedOption value here.
+        console.log('Selected option:', selectedOption);
     };
 
     return (
         <div>
-            <h2>Contact Us</h2>
+            <h2>What would you like to protect?</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    <label>Name:</label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="business"
+                            value="Busines"
+                            checked={selectedOption === 'business'}
+                            onChange={handleOptionChange}
+                        />
+                        Business name or product
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            name="logo"
+                            value="Logo"
+                            checked={selectedOption === 'logo'}
+                            onChange={handleOptionChange}
+                        />
+                        Stylized logo
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            name="slogan"
+                            value="Slogan"
+                            checked={selectedOption === 'slogan'}
+                            onChange={handleOptionChange}
+                        />
+                        Slogan or tagline
+                    </label>
+                </div>
+ //////////////////////////////////////////////////////////////////////////
+                <h2 className='mt-4'>What would you like to trademark?</h2>
+                <div>
                     <input
                         type="text"
                         name="name"
-                        value={formData.name}
-                        onChange={handleChange}
+                        placeholder={"enter mark"}
+                        // value={formData.name}
+                        // onChange={handleChange}
                     />
+
+                </div>
+////////////////////////////////////////////////////////////////////////
+                    <h2>Are you currently using your trademark to provide products or services to consumers?</h2>
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            name="business"
+                            value="Busines"
+                            checked={selectedOption === 'business'}
+                            onChange={handleOptionChange}
+                        />
+                        Yes
+                    </label>
                 </div>
                 <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                    />
+                    <label>
+                        <input
+                            type="radio"
+                            name="logo"
+                            value="Logo"
+                            checked={selectedOption === 'logo'}
+                            onChange={handleOptionChange}
+                        />
+                        No
+                    </label>
                 </div>
-                <div>
-                    <label>Message:</label>
-                    <textarea
-                        name="message"
-                        value={formData.message}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit">Submit</button>
+
+
+
+
+                *////////////////////////////////////////////////////////////////////////*
+                <button type="submit" className="mt-4">Submit</button>
             </form>
         </div>
     );
